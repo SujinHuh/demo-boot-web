4219a233184ab5970fea293ca6fc827ae7f7e16e
수진저장소 <43295876+SujinHuh@users.noreply.github.com>
2019-12-14 18:50:43 +0900

    Formatter set
    Spring Boot를 사용할 때 Formatter가 Bean으로 등록이 되어 있다면 Bean으로 등록을 해준다.

--- src/test/java/me/whiteship/demobootweb/SampleControllerTest.java
+++ src/test/java/me/whiteship/demobootweb/SampleControllerTest.java
@@ -3,18 +3,19 @@ package me.whiteship.demobootweb;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@SpringBootTest
+@AutoConfigureMockMvc
 public class SampleControllerTest {
 
     @Autowired
@@ -22,7 +23,8 @@ public class SampleControllerTest {
 
     @Test
     public void hello() throws Exception {
-        this.mockMvc.perform(get("/hello").param("name ", "Sujin" ))
+        this.mockMvc.perform(get("/hello")
+                .param("name ", "Sujin" ))
                 .andDo(print())
                 .andExpect(content().string("hello Sujin"));
 
